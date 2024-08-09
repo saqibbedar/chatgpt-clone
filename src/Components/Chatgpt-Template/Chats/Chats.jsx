ae6b5a290b3f392a65ab76b16ace0b325f69a5ce
saqibbedar <Chandio.Saqib.M@gmail.com>
2024-08-09 18:04:06 +0500

    update chats component

--- src/Components/Chatgpt-Template/Chats/Chats.jsx
+++ src/Components/Chatgpt-Template/Chats/Chats.jsx
@@ -3,17 +3,17 @@ import { assets } from "../../../assets/assets"
 const Chats = ({isClient, ClientQuery, isGPT, gptResponse}) => {
   return (
     <>
-      <div className="client-query bg-gptInputBg p-5 rounded-3xl w-[65%]">
+        <div className={`client-query ${isClient? 'block' : 'hidden'} bg-gptInputBg p-5 rounded-3xl w-[65%]`}>
             <p className="text-gptColor/[.9]">{isClient && ClientQuery}</p>
-          </div>
+        </div>
 
-          <div className="gpt-response flex items-start gap-4">
+        <div className={`gpt-response ${isGPT? 'flex' : 'hidden'} items-start gap-4`}>
 
             <img src={assets.gptLogo} className="w-10 h-10 p-2 border-gptBorderColor border rounded-full" alt="" />
 
             <div className="generated-response">
-              <p className="text-gptColor/[.9]">{isGPT && gptResponse}</p>
-              <div className="copy-btn mt-3 cursor-pointer w-[fit-content]" title="copy"><img src={assets.copy} alt="" className="w-[1.1rem]"/></div>
+                <p className="text-gptColor/[.9]">{isGPT && gptResponse}</p>
+                <div className="copy-btn mt-3 cursor-pointer w-[fit-content]" title="copy"><img src={assets.copy} alt="" className="w-[1.1rem]"/></div>
             </div>
 
         </div>
